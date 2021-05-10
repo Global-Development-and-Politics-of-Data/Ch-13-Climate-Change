ebbb3a168f3d14a0d529f444257d3f3bd7c081fe
Noorah Alhasan <alhasan.noorah@gmail.com>
2021-05-10 18:41:11 -0500

    updated scripts

---
+++ scripts/analysis/unfccc-emissions-analysis.R
@@ -0,0 +1,126 @@
+## ----setup, include=FALSE--------------------------------------------------------------------------------------------
+knitr::opts_chunk$set(echo = TRUE, 
+                      warning = FALSE,
+                      purl = TRUE,
+                      out.extra = '',
+                      tab.cap.pre = "Table ", tab.cap.sep = ": ",
+                      knitr.duplicate.label = "allow",
+                      tidy.opts = list(width.cutoff = 40), 
+                      tidy = TRUE,
+                      strip.white = FALSE)
+
+
+
+## ----eval=FALSE, warning=FALSE, message=FALSE------------------------------------------------------------------------
+## install.packages("here")
+## install.packages("tidyverse")
+## install.packages("janitor")
+## install.packages("knitr")
+## install.packages("flextable")
+## install.packages("officer")
+## install.packages("extrafont")
+## # font_import() # Only do this once
+
+
+## ----load libraries, warning=FALSE, message=FALSE--------------------------------------------------------------------
+library(here)
+library(tidyverse)
+library(janitor)
+library(knitr)
+library(flextable)
+library(officer)
+library(extrafont)
+loadfonts(device = "win")
+
+
+## ----run script for clean dataset, warning=FALSE, message=FALSE------------------------------------------------------
+source(here("scripts/cleaning", "unfccc-emissions-clean.R"))
+
+
+## ----import dataset, warning=FALSE, message=FALSE--------------------------------------------------------------------
+unfccc_emissions <- read.csv(here("scripts/cleaning", "unfccc-emissions-clean.csv"))
+
+
+## ----check NAs CO2 non-annex, warning=FALSE, message=FALSE, caption = "Percent of missing values of CO~2~ over time (Non-Annex I)"----
+table_00 <- unfccc_emissions %>% 
+  dplyr::ungroup() %>% 
+  dplyr::filter(group == "Non-Annex I") %>% 
+  dplyr::group_by(year) %>% 
+  dplyr::summarise(missing = round(sum(is.na(co2))/length(co2) * 100, digits = 2)) %>%
+  add_row(year = "2019", missing = NA)
+
+table_01 <- cbind(table_00[table_00$year < 2000, ], table_00[table_00$year >= 2000 & table_00$year < 2010, ], table_00[table_00$year >= 2010 & table_00$year <= 2019, ]) %>%
+  repair_names()
+
+autonum <- run_autonum(seq_id = "tab", bkm = "TC1", bkm_all = TRUE)
+
+table_01 %>%
+  flextable::flextable() %>%
+  set_header_labels(year = "Year", missing = "Missing (%)", year1 = "Year", missing1 = "Missing (%)", year2 = "Year", missing2 = "Missing (%)") %>%
+  theme_vanilla() %>%
+  font(fontname = "Times New Roman", part = "all") %>%
+  set_caption("Percent of missing values of CO~2~ over time (Non-Annex I)", autonum = autonum) %>%
+  highlight(j = "missing", i = ~ missing < 50, color = "yellow") %>%
+  highlight(j = "missing1", i = ~ missing1 < 50, color = "yellow") %>%
+  highlight(j = "missing2", i = ~ missing2 < 50, color = "yellow") %>%
+  highlight(j = "missing", i = ~ missing > 90, color = "lightcoral") %>%
+  highlight(j = "missing1", i = ~ missing1 > 90, color = "lightcoral") %>%
+  highlight(j = "missing2", i = ~ missing2 > 90, color = "lightcoral") %>%
+  autofit()
+
+
+## ----check NAs CO2 annex, warning=FALSE, message=FALSE, caption = "Percent of missing values of CO~2~ over time (Annex I)"----
+table_00 <- unfccc_emissions %>% 
+  dplyr::ungroup() %>% 
+  dplyr::filter(group == "Annex I") %>% 
+  dplyr::group_by(year) %>% 
+  dplyr::summarise(missing = round(sum(is.na(co2))/length(co2) * 100, digits = 2)) %>%
+  add_row(year = "2019", missing = NA)
+
+table_01 <- cbind(table_00[table_00$year < 2000, ], table_00[table_00$year >= 2000 & table_00$year < 2010, ], table_00[table_00$year >= 2010 & table_00$year <= 2019, ]) %>%
+  repair_names()
+
+
+table_01 %>%
+  flextable::flextable() %>%
+  set_header_labels(year = "Year", missing = "Missing (%)", year1 = "Year", missing1 = "Missing (%)", year2 = "Year", missing2 = "Missing (%)") %>%
+  font(fontname = "Times New Roman", part = "all") %>%
+  set_caption("Percent of missing values of CO~2~ over time (Annex I)", autonum = autonum) %>%
+  theme_vanilla() %>%
+  autofit()
+
+
+## ----check NAs CO2 by region non-annex, warning=FALSE, message=FALSE-------------------------------------------------
+missing_non_annex_i_region <- unfccc_emissions %>% 
+  dplyr::ungroup() %>% 
+  dplyr::filter(group == "Non-Annex I") %>%
+  dplyr::mutate(time_period = floor(as.numeric(as.character(year))/10)*10) %>%
+  dplyr::group_by(time_period, region) %>%
+  dplyr::summarise(missing = sum(is.na(co2))/length(co2) * 100) %>%
+  dplyr::mutate(time_period = as.factor(time_period))
+  
+missing_non_annex_i_region$time_period <- dplyr::recode_factor(missing_non_annex_i_region$time_period, `1990` = "1990-1999", `2000` = "2000-2009", `2010` = "2010-2018")
+
+
+## ----barplot by decade Non-Annex I, warning=FALSE, message=FALSE, caption = "Percent of missing values of CO~2~ by region (Non-Annex I)", fig.width = 8----
+missing_non_annex_i_region %>%
+  ggplot2::ggplot(aes(x = time_period, y = missing, fill = str_wrap(region, 15))) +
+  ggplot2::geom_bar(stat="identity", position=position_dodge(), color = "black") +
+  ggplot2::theme_classic() +
+  ggplot2::ylab("Missing Values (%)") +
+  ggplot2::xlab("Years") +
+  ggplot2::coord_flip() +
+  ggplot2::scale_fill_brewer(palette = "Paired", name = "Regions") +
+  ggplot2::geom_text(aes(label = round(missing, digits = 0)), position=position_dodge(width=0.9), hjust = -0.25, size = 4) +
+  ggplot2::theme(text=element_text(family = "Arial", size = 11), 
+                 axis.title.x = element_text(family = "Arial", size = 12), 
+                 axis.title.y = element_text(family = "Arial", size = 12), 
+                 legend.text=element_text(family = "Arial", size=11))
+
+ggsave(here("images", "missing-non-annex-i-01.svg"), device="svg", dpi=300)  
+
+
+## ----export as an R script, eval=FALSE, message=FALSE, warning=FALSE-------------------------------------------------
+## knitr::purl("unfccc-emissions-analysis.Rmd", "unfccc-emissions-analysis.R")
+## knitr::write_bib(.packages(), "packages.bib")
+
--- scripts/cleaning/unfccc-emissions-clean.R
+++ scripts/cleaning/unfccc-emissions-clean.R
@@ -1,15 +1,16 @@
-## ----setup, include=FALSE----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+## ----setup, include=FALSE------------------------------------------------------------------------------------------
 knitr::opts_chunk$set(echo = TRUE, 
                       warning = FALSE,
                       purl = TRUE,
                       out.extra = '',
                       knitr.duplicate.label = "allow",
-                      tidy.opts=list(width.cutoff=50), 
-                      tidy=TRUE)
+                      tidy.opts = list(width.cutoff = 40), 
+                      tidy = TRUE,
+                      strip.white = FALSE)
 
 
 
-## ----eval=FALSE, warning=FALSE, message=FALSE--------------------------------------------------------------------------------------------------------------------------------------------------------------
+## ----eval=FALSE, warning=FALSE, message=FALSE----------------------------------------------------------------------
 ## install.packages("here")
 ## install.packages("openxlsx")
 ## install.packages("janitor")
@@ -19,7 +20,7 @@ knitr::opts_chunk$set(echo = TRUE,
 ## install.packages("knitr")
 
 
-## ----load libraries, warning=FALSE, message=FALSE----------------------------------------------------------------------------------------------------------------------------------------------------------
+## ----load libraries, warning=FALSE, message=FALSE------------------------------------------------------------------
 library(here)
 library(openxlsx)
 library(janitor)
@@ -29,67 +30,67 @@ library(fuzzyjoin)
 library(knitr)
 
 
-## ----import data (Annex I)---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-annex_i_emissions <- openxlsx::read.xlsx(here("data", "annual-net-emissions-removals-annex-i-raw.xlsx"),
+## ----import data (Annex I)-----------------------------------------------------------------------------------------
+annex_i_emissions <- openxlsx::read.xlsx(here::here("data", "annual-net-emissions-removals-annex-i-raw.xlsx"),
                                          startRow = 5, 
                                          fillMergedCells = TRUE, 
                                          rows = 5:96) %>% 
   janitor::clean_names()
 
 
-## ----import data (Non-Annex I)-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-non_annex_i_emissions <- openxlsx::read.xlsx(here("data", "annual-net-emissions-removals-non-annex-i-raw.xlsx"),
+## ----import data (Non-Annex I)-------------------------------------------------------------------------------------
+non_annex_i_emissions <- openxlsx::read.xlsx(here::here("data", "annual-net-emissions-removals-non-annex-i-raw.xlsx"),
                                              startRow = 5, 
                                              fillMergedCells = TRUE, 
-                                             rows = 5:96) %>% 
+                                             rows = 5:302) %>% 
   janitor::clean_names()
 
 
-## ----clean main dataframe (Annex I), warning=FALSE, message=FALSE------------------------------------------------------------------------------------------------------------------------------------------
+## ----clean main dataframe (Annex I), warning=FALSE, message=FALSE--------------------------------------------------
 annex_i_emissions <- annex_i_emissions[-c(1),]
 
-colnames(annex_i_emissions)[1] <- "party"
+colnames(annex_i_emissions)[1] <- "country"
 
 annex_i_emissions <- annex_i_emissions %>% 
   dplyr::rename_all(funs(stringr::str_replace_all(., "aggregate_gh_gs", "ghg")))
 
 
-## ----clean main dataframe (Non-Annex I), warning=FALSE, message=FALSE--------------------------------------------------------------------------------------------------------------------------------------
+## ----clean main dataframe (Non-Annex I), warning=FALSE, message=FALSE----------------------------------------------
 non_annex_i_emissions <- non_annex_i_emissions[-c(1),]
 
-colnames(non_annex_i_emissions)[1] <- "party"
+colnames(non_annex_i_emissions)[1] <- "country"
 
 non_annex_i_emissions <- non_annex_i_emissions %>% 
   dplyr::rename_all(funs(stringr::str_replace_all(., "aggregate_gh_gs", "ghg")))
 
 
-## ----disaggregate dataframe by gas type (Annex I)----------------------------------------------------------------------------------------------------------------------------------------------------------
+## ----disaggregate dataframe by gas type (Annex I)------------------------------------------------------------------
 annex_i_emissions_co2 <- annex_i_emissions %>% 
-  dplyr::select("party", "gas", contains("co2")) %>% 
+  dplyr::select("country", "gas", contains("co2")) %>% 
   dplyr::rename("base_year" = "co2")
 
 colnames(annex_i_emissions_co2)[4:32] <- 1990:2018
 
 annex_i_emissions_ghg <- annex_i_emissions %>% 
-  dplyr::select("party", "gas", contains("ghg")) %>% 
+  dplyr::select("country", "gas", contains("ghg")) %>% 
   dplyr::rename("base_year" = "ghg")
 
 colnames(annex_i_emissions_ghg)[4:32] <- 1990:2018
 
 
-## ----disaggregate dataframe by gas type (Non-Annex I)------------------------------------------------------------------------------------------------------------------------------------------------------
+## ----disaggregate dataframe by gas type (Non-Annex I)--------------------------------------------------------------
 non_annex_i_emissions_co2 <- non_annex_i_emissions %>% 
-  dplyr::select("party", "gas", contains("co2"))
+  dplyr::select("country", "gas", contains("co2"))
 
 colnames(non_annex_i_emissions_co2)[3:31] <- 1990:2018
 
 non_annex_i_emissions_ghg <- non_annex_i_emissions %>% 
-  dplyr::select("party", "gas", contains("ghg"))
+  dplyr::select("country", "gas", contains("ghg"))
 
 colnames(non_annex_i_emissions_ghg)[3:31] <- 1990:2018
 
 
-## ----convert datasets to long (Annex I)--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+## ----convert datasets to long (Annex I)----------------------------------------------------------------------------
 annex_i_emissions_co2 <- annex_i_emissions_co2 %>% 
   tidyr::gather(year, co2, base_year:`2018`) %>% 
   mutate(group = "Annex I")
@@ -99,7 +100,7 @@ annex_i_emissions_ghg <- annex_i_emissions_ghg %>%
   mutate(group = "Annex I")
 
 
-## ----convert datasets to long (Non-Annex I)----------------------------------------------------------------------------------------------------------------------------------------------------------------
+## ----convert datasets to long (Non-Annex I)------------------------------------------------------------------------
 non_annex_i_emissions_co2 <- non_annex_i_emissions_co2 %>% 
   tidyr::gather(year, co2, `1990`:`2018`) %>% 
   mutate(group = "Non-Annex I")
@@ -109,40 +110,79 @@ non_annex_i_emissions_ghg <- non_annex_i_emissions_ghg %>%
   mutate(group = "Non-Annex I")
 
 
-## ----merge datasets back (Annex I)-------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+## ----merge datasets back (Annex I)---------------------------------------------------------------------------------
 annex_i_emissions <- annex_i_emissions_ghg %>% 
-  dplyr::full_join(annex_i_emissions_co2, by =c("party", "gas", "year", "group"))
+  dplyr::full_join(annex_i_emissions_co2, by =c("country", "gas", "year", "group"))
 
 rm(annex_i_emissions_co2,annex_i_emissions_ghg)
 
 
-## ----merge datasets back (Non-Annex I)---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+## ----merge datasets back (Non-Annex I)-----------------------------------------------------------------------------
 non_annex_i_emissions <- non_annex_i_emissions_ghg %>% 
-  dplyr::full_join(non_annex_i_emissions_co2, by =c("party", "gas", "year", "group"))
+  dplyr::full_join(non_annex_i_emissions_co2, by =c("country", "gas", "year", "group"))
 
 rm(non_annex_i_emissions_co2,non_annex_i_emissions_ghg)
 
 
-## ----Merge Annex I and Non-Annex I dataframes, message=FALSE, warning=FALSE--------------------------------------------------------------------------------------------------------------------------------
+## ----Merge Annex I and Non-Annex I dataframes, message=FALSE, warning=FALSE----------------------------------------
 unfccc_emissions <- dplyr::bind_rows(annex_i_emissions,non_annex_i_emissions) %>%
   dplyr::rename("type" = "gas") %>% 
-  dplyr::mutate(type = stringr::str_replace(type,"Total GHG emissions excluding LULUCF/LUCF", "Total GHG emissions without LULUCF")) %>% 
-  dplyr::mutate(type = stringr::str_replace(type,"Total GHG emissions including LULUCF/LUCF", "Total GHG emissions with LULUCF")) %>%
+  dplyr::mutate(type = stringr::str_replace(type, 
+                                            "Total GHG emissions excluding LULUCF/LUCF", 
+                                            "Total GHG emissions without LULUCF")) %>% 
+  dplyr::mutate(type = stringr::str_replace(type, 
+                                            "Total GHG emissions including LULUCF/LUCF", 
+                                            "Total GHG emissions with LULUCF")) %>%
   readr::type_convert() %>%
   dplyr::mutate_if(is.character, as.factor)
 
 
-## ----Merge region dataframe with unfccc_emissions datafram, message=FALSE, warning=FALSE-------------------------------------------------------------------------------------------------------------------
-region <- countrycode::codelist %>% select(country.name.en, un.region.name) %>% mutate(country.name.en = stringr::str_replace_all(country.name.en, c("Antigua & Barbuda" = "Antigua and Barbuda", "Bosnia & Herzegovina" = "Bosnia and Herzegovina", "Cape Verde" = "Cabo Verde", "Congo - Kinshasa" = "Congo", "Côte d’Ivoire" = "Cote d'Ivoire", "Congo - Brazzaville" = "Democratic Republic of Congo", "North Korea" = "Democratic People's Republic of Korea")))
+## ----Merge region dataframe with unfccc_emissions dataframe, message=FALSE, warning=FALSE--------------------------
+region <- countrycode::codelist %>% 
+  dplyr::select(country.name.en, region, iso3c) %>% 
+  dplyr::mutate(country.name.en = stringr::str_replace_all(country.name.en, 
+                                                    c("Antigua & Barbuda" = "Antigua and Barbuda", 
+                                                      "Bosnia & Herzegovina" = "Bosnia and Herzegovina", 
+                                                      "Cape Verde" = "Cabo Verde", 
+                                                      "Congo - Kinshasa" = "Congo", 
+                                                      "Côte d’Ivoire" = "Cote d'Ivoire", 
+                                                      "Congo - Brazzaville" = "Democratic Republic of Congo",
+                                                      "Laos" = "Lao People's Democratic Republic",
+                                                      "Micronesia \\(Federated States of\\)" = "Micronesia",
+                                                      "Myanmar \\(Burma\\)" = "Myanmar",
+                                                      "St. Lucia" = "Saint Lucia",
+                                                      "St. Vincent & Grenadines" = "Saint Vincent and the Grenadines",
+                                                      "São Tomé & Príncipe" = "Sao Tome and Principe",
+                                                      "Palestinian Territories" = "State of Palestine",
+                                                      "Trinidad & Tobago" = "Trinidad and Tobago",
+                                                      "North Korea" = "Democratic People's Republic of Korea",
+                                                      "South Korea" = "Republic of Korea",
+                                                      "St. Kitts & Nevis" = "Saint Kitts and Nevis",
+                                                      "Vietnam" = "Viet Nam")))
 
-unfccc_emissions <- unfccc_emissions %>% fuzzyjoin::regex_left_join(region, by= c(party = "country.name.en")) %>% dplyr::rename(region = un.region.name) %>% select(-country.name.en)
-unfccc_emissions$region[unfccc_emissions$party %in% c("European Union (Convention)", "European Union (KP)")] <- "Europe"
+unfccc_emissions <- unfccc_emissions %>% 
+  fuzzyjoin::regex_left_join(region, by = c(country = "country.name.en")) %>%
+  dplyr::rename(iso = iso3c) %>% 
+  dplyr::filter(!(country == "United Kingdom of Great Britain and Northern Ireland" & iso == "IRL"),
+                !(country == "Guinea-Bissau" & iso == "GIN"),
+                !(country == "Papua New Guinea" & iso == "GIN"),
+                !(country == "Democratic People's Republic of Korea" & iso == "KOR"),
+                !(country == "Dominican Republic" & iso == "DMA"),
+                !(country == "Nigeria" & iso == "NER"),
+                !(country == "South Sudan" & iso == "SDN")) %>%
+  dplyr::select(-country.name.en) %>%
+  dplyr::relocate(iso, .before = country) %>%
+  dplyr::relocate(c(region, group, year), .after = country)
+  
+
+unfccc_emissions$region[unfccc_emissions$country %in% c("European Union (Convention)", "European Union (KP)")] <- "Europe & Central Asia"
 
 
-## ----export as a csv file----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-write.csv(unfccc_emissions, "unfccc-emissions-clean.csv")
+## ----export as a csv file------------------------------------------------------------------------------------------
+utils::write.csv(unfccc_emissions, "unfccc-emissions-clean.csv", row.names = FALSE)
 
 
-## ----export as an R script, eval=FALSE, message=FALSE, warning=FALSE---------------------------------------------------------------------------------------------------------------------------------------
+## ----export as an R script, eval=FALSE, message=FALSE, warning=FALSE-----------------------------------------------
 ## knitr::purl("unfccc-emissions-clean.Rmd", "unfccc-emissions-clean.R")
+## knitr::write_bib(.packages(), "packages.bib")
 
